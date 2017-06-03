8ddb2c346549d43b8c5b1c0c4c1ee01c0c5ada2b
Yuta Naito <t415036@st.pu-toyama.ac.jp>
2017-06-03 11:59:11 +0900

    2月・3月の飲食費のグラフ(20代・20代未満)

--- eatcost_feb.js
+++ eatcost_feb.js
@@ -1,14 +1,14 @@
-var eatCost_man = [0, 0, 0, 0, 0, 0];     //男：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_woman = [0, 0, 0, 0, 0, 0];   //女：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
+var eatCost_man = [0, 0, 0, 0, 0, 0];     //男：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_woman = [0, 0, 0, 0, 0, 0];   //女：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
 
-var eatCost_10 = [0, 0, 0, 0, 0, 0];      //10代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_20 = [0, 0, 0, 0, 0, 0];      //20代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_30 = [0, 0, 0, 0, 0, 0];      //30代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_40 = [0, 0, 0, 0, 0, 0];      //40代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_50 = [0, 0, 0, 0, 0, 0];      //50代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_60 = [0, 0, 0, 0, 0, 0];      //60代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_70 = [0, 0, 0, 0, 0, 0];      //70代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_80 = [0, 0, 0, 0, 0, 0];      //80代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
+var eatCost_10 = [0, 0, 0, 0, 0, 0];      //10代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_20 = [0, 0, 0, 0, 0, 0];      //20代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_30 = [0, 0, 0, 0, 0, 0];      //30代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_40 = [0, 0, 0, 0, 0, 0];      //40代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_50 = [0, 0, 0, 0, 0, 0];      //50代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_60 = [0, 0, 0, 0, 0, 0];      //60代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_70 = [0, 0, 0, 0, 0, 0];      //70代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_80 = [0, 0, 0, 0, 0, 0];      //80代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
 
 $(function(){
   $.ajax({url: 'data_february.json', dataType: 'json', async: false}).done(function(data){
@@ -225,9 +225,26 @@ $(function(){
   eatCost_man[1] = eatCost_man[0] + eatCost_man[1];
   eatCost_woman[1] = eatCost_woman[0] + eatCost_woman[1];
 
-  eatCost_mans = [[ '0-1000円以下', eatCost_man[1] ], [ '1001~2000円以下', eatCost_man[2] ], [ '2001~3000円以下', eatCost_man[3] ], [ '3001~5000円以下', eatCost_man[4] ], [ '5001円以上', eatCost_man[5] ]];
-  eatCost_womans = [[ '0-1000円以下', eatCost_woman[1] ], [ '1001~2000円以下', eatCost_woman[2] ], [ '2001~3000円以下', eatCost_woman[3] ], [ '3001~5000円以下', eatCost_woman[4] ], [ '5001円以上', eatCost_woman[5] ]];
+  eatCost_10[1] = eatCost_10[0] + eatCost_10[1];
+  eatCost_20[1] = eatCost_20[0] + eatCost_20[1];
+  eatCost_30[1] = eatCost_30[0] + eatCost_30[1];
+  eatCost_40[1] = eatCost_40[0] + eatCost_40[1];
+  eatCost_50[1] = eatCost_50[0] + eatCost_50[1];
+  eatCost_60[1] = eatCost_60[0] + eatCost_60[1];
+  eatCost_70[1] = eatCost_70[0] + eatCost_70[1];
+  eatCost_80[1] = eatCost_80[0] + eatCost_80[1];
 
+  eatCost_mans = [[ '0-1000円', eatCost_man[1] ], [ '1001~2000円', eatCost_man[2] ], [ '2001~3000円', eatCost_man[3] ], [ '3001~5000円', eatCost_man[4] ], [ '5001円以上', eatCost_man[5] ]];
+  eatCost_womans = [[ '0-1000円', eatCost_woman[1] ], [ '1001~2000円', eatCost_woman[2] ], [ '2001~3000円', eatCost_woman[3] ], [ '3001~5000円', eatCost_woman[4] ], [ '5001円以上', eatCost_woman[5] ]];
+
+  eatCost_10year = [[ '0-1000円', eatCost_10[1] ], [ '1001~2000円', eatCost_10[2] ], [ '2001~3000円', eatCost_10[3] ], [ '3001~5000円', eatCost_10[4] ], [ '5001円以上', eatCost_10[5] ]];
+  eatCost_20year = [[ '0-1000円', eatCost_20[1] ], [ '1001~2000円', eatCost_20[2] ], [ '2001~3000円', eatCost_20[3] ], [ '3001~5000円', eatCost_20[4] ], [ '5001円以上', eatCost_20[5] ]];
+  eatCost_30year = [[ '0-1000円', eatCost_30[1] ], [ '1001~2000円', eatCost_30[2] ], [ '2001~3000円', eatCost_30[3] ], [ '3001~5000円', eatCost_30[4] ], [ '5001円以上', eatCost_30[5] ]];
+  eatCost_40year = [[ '0-1000円', eatCost_40[1] ], [ '1001~2000円', eatCost_40[2] ], [ '2001~3000円', eatCost_40[3] ], [ '3001~5000円', eatCost_40[4] ], [ '5001円以上', eatCost_40[5] ]];
+  eatCost_50year = [[ '0-1000円', eatCost_50[1] ], [ '1001~2000円', eatCost_50[2] ], [ '2001~3000円', eatCost_50[3] ], [ '3001~5000円', eatCost_50[4] ], [ '5001円以上', eatCost_50[5] ]];
+  eatCost_60year = [[ '0-1000円', eatCost_60[1] ], [ '1001~2000円', eatCost_60[2] ], [ '2001~3000円', eatCost_60[3] ], [ '3001~5000円', eatCost_60[4] ], [ '5001円以上', eatCost_60[5] ]];
+  eatCost_70year = [[ '0-1000円', eatCost_70[1] ], [ '1001~2000円', eatCost_70[2] ], [ '2001~3000円', eatCost_70[3] ], [ '3001~5000円', eatCost_70[4] ], [ '5001円以上', eatCost_70[5] ]];
+  eatCost_80year = [[ '0-1000円', eatCost_80[1] ], [ '1001~2000円', eatCost_80[2] ], [ '2001~3000円', eatCost_80[3] ], [ '3001~5000円', eatCost_80[4] ], [ '5001円以上', eatCost_80[5] ]];
 
   jQuery.jqplot(
       'february_eatcost',
@@ -281,6 +298,70 @@ $(function(){
           },
           title: {
                 text: '2月の男女別飲食費の比較',
+                show: true,
+                fontFamily: 'ＭＳ ゴシック',
+                fontSize: '20px',
+                textAlign: 'center',
+                textColor: 'black',
+                location: 's',
+            }
+      }
+  );
+
+  jQuery.jqplot(
+      'feb_eatcost_10and20',
+      [
+          eatCost_10year, eatCost_20year
+      ],
+      {
+          animate: true,
+
+          seriesDefaults: {
+              renderer: jQuery . jqplot . BarRenderer,
+
+              pointLabels: {
+                   show: true,
+                   location: 'n',
+                   ypadding: -3,
+                   escapeHTML: false,
+                   formatString: '<b style="color: blue;">%d</b>'
+               }
+          },
+          series: [
+             { label: '20歳未満' },
+             { label: '20代' },
+            //  { label: '30代'},
+            //  { label: '40代'},
+
+          ],
+          legend: {
+                show: true,
+                placement: 'outsideGrid',
+                location: 'e',
+                renderer: jQuery . jqplot . EnhancedLegendRenderer,
+                rendererOptions: {
+                    numberColumns: 3
+                }
+            },
+
+
+          seriesColors:[ '#FF0000', '#FFA500'],
+
+          axes: {
+              xaxis: {
+                  renderer: jQuery . jqplot . CategoryAxisRenderer,
+
+              },
+              // 縦軸(y軸)
+              yaxis:{
+                  label: '人数(人)',
+                  min: 0,
+                  max: 50,
+                  tickInterval: 10,
+              }
+          },
+          title: {
+                text: '2月の飲食費の比較(20代未満と20代)',
                 show: true,
                 fontFamily: 'ＭＳ ゴシック',
                 fontSize: '20px',
--- eatcost_march.js
+++ eatcost_march.js
@@ -1,14 +1,14 @@
-var eatCost_man2 = [0, 0, 0, 0, 0, 0];     //男：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_woman2 = [0, 0, 0, 0, 0, 0];   //女：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
+var eatCost_man2 = [0, 0, 0, 0, 0, 0];     //男：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_woman2 = [0, 0, 0, 0, 0, 0];   //女：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
 
-var eatCost_10_2 = [0, 0, 0, 0, 0, 0];      //10代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_20_2 = [0, 0, 0, 0, 0, 0];      //20代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_30_2 = [0, 0, 0, 0, 0, 0];      //30代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_40_2 = [0, 0, 0, 0, 0, 0];      //40代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_50_2 = [0, 0, 0, 0, 0, 0];      //50代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_60_2 = [0, 0, 0, 0, 0, 0];      //60代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_70_2 = [0, 0, 0, 0, 0, 0];      //70代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
-var eatCost_80_2 = [0, 0, 0, 0, 0, 0];      //80代：左から順に、無し・1000円以下・1001~2000円以下・2001~3000円以下・3001~5000円以下・5000円以上
+var eatCost_10_2 = [0, 0, 0, 0, 0, 0];      //10代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_20_2 = [0, 0, 0, 0, 0, 0];      //20代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_30_2 = [0, 0, 0, 0, 0, 0];      //30代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_40_2 = [0, 0, 0, 0, 0, 0];      //40代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_50_2 = [0, 0, 0, 0, 0, 0];      //50代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_60_2 = [0, 0, 0, 0, 0, 0];      //60代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_70_2 = [0, 0, 0, 0, 0, 0];      //70代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
+var eatCost_80_2 = [0, 0, 0, 0, 0, 0];      //80代：左から順に、無し・1000円・1001~2000円・2001~3000円・3001~5000円・5000円以上
 
 $(function(){
   $.ajax({url: 'data_march.json', dataType: 'json', async: false}).done(function(data){
@@ -225,9 +225,26 @@ $(function(){
   eatCost_man2[1] = eatCost_man2[0] + eatCost_man2[1];
   eatCost_woman2[1] = eatCost_woman2[0] + eatCost_woman2[1];
 
-  eatCost_man2 = [[ '0-1000円以下', eatCost_man2[1] ], [ '1001~2000円以下', eatCost_man2[2] ], [ '2001~3000円以下', eatCost_man2[3] ], [ '3001~5000円以下', eatCost_man2[4] ], [ '5001円以上', eatCost_man2[5] ]];
-  eatCost_woman2 = [[ '0-1000円以下', eatCost_woman2[1] ], [ '1001~2000円以下', eatCost_woman2[2] ], [ '2001~3000円以下', eatCost_woman2[3] ], [ '3001~5000円以下', eatCost_woman2[4] ], [ '5001円以上', eatCost_woman2[5] ]];
+  eatCost_10_2[1] = eatCost_10_2[0] + eatCost_10_2[1];
+  eatCost_20_2[1] = eatCost_20_2[0] + eatCost_20_2[1];
+  eatCost_30_2[1] = eatCost_30_2[0] + eatCost_30_2[1];
+  eatCost_40_2[1] = eatCost_40_2[0] + eatCost_40_2[1];
+  eatCost_50_2[1] = eatCost_50_2[0] + eatCost_50_2[1];
+  eatCost_60_2[1] = eatCost_60_2[0] + eatCost_60_2[1];
+  eatCost_70_2[1] = eatCost_70_2[0] + eatCost_70_2[1];
+  eatCost_80_2[1] = eatCost_80_2[0] + eatCost_80_2[1];
 
+  eatCost_man2 = [[ '0-1000円', eatCost_man2[1] ], [ '1001~2000円', eatCost_man2[2] ], [ '2001~3000円', eatCost_man2[3] ], [ '3001~5000円', eatCost_man2[4] ], [ '5001円以上', eatCost_man2[5] ]];
+  eatCost_woman2 = [[ '0-1000円', eatCost_woman2[1] ], [ '1001~2000円', eatCost_woman2[2] ], [ '2001~3000円', eatCost_woman2[3] ], [ '3001~5000円', eatCost_woman2[4] ], [ '5001円以上', eatCost_woman2[5] ]];
+
+  eatCost_10_2year = [[ '0-1000円', eatCost_10_2[1] ], [ '1001~2000円', eatCost_10_2[2] ], [ '2001~3000円', eatCost_10_2[3] ], [ '3001~5000円', eatCost_10_2[4] ], [ '5001円以上', eatCost_10_2[5] ]];
+  eatCost_20_2year = [[ '0-1000円', eatCost_20_2[1] ], [ '1001~2000円', eatCost_20_2[2] ], [ '2001~3000円', eatCost_20_2[3] ], [ '3001~5000円', eatCost_20_2[4] ], [ '5001円以上', eatCost_20_2[5] ]];
+  eatCost_30_2year = [[ '0-1000円', eatCost_30_2[1] ], [ '1001~2000円', eatCost_30_2[2] ], [ '2001~3000円', eatCost_30_2[3] ], [ '3001~5000円', eatCost_30_2[4] ], [ '5001円以上', eatCost_30_2[5] ]];
+  eatCost_40_2year = [[ '0-1000円', eatCost_40_2[1] ], [ '1001~2000円', eatCost_40_2[2] ], [ '2001~3000円', eatCost_40_2[3] ], [ '3001~5000円', eatCost_40_2[4] ], [ '5001円以上', eatCost_40_2[5] ]];
+  eatCost_50_2year = [[ '0-1000円', eatCost_50_2[1] ], [ '1001~2000円', eatCost_50_2[2] ], [ '2001~3000円', eatCost_50_2[3] ], [ '3001~5000円', eatCost_50_2[4] ], [ '5001円以上', eatCost_50_2[5] ]];
+  eatCost_60_2year = [[ '0-1000円', eatCost_60_2[1] ], [ '1001~2000円', eatCost_60_2[2] ], [ '2001~3000円', eatCost_60_2[3] ], [ '3001~5000円', eatCost_60_2[4] ], [ '5001円以上', eatCost_60_2[5] ]];
+  eatCost_70_2year = [[ '0-1000円', eatCost_70_2[1] ], [ '1001~2000円', eatCost_70_2[2] ], [ '2001~3000円', eatCost_70_2[3] ], [ '3001~5000円', eatCost_70_2[4] ], [ '5001円以上', eatCost_70_2[5] ]];
+  eatCost_80_2year = [[ '0-1000円', eatCost_80_2[1] ], [ '1001~2000円', eatCost_80_2[2] ], [ '2001~3000円', eatCost_80_2[3] ], [ '3001~5000円', eatCost_80_2[4] ], [ '5001円以上', eatCost_80_2[5] ]];
 
   jQuery.jqplot(
       'march_eatcost',
@@ -281,6 +298,70 @@ $(function(){
           },
           title: {
                 text: '3月の男女別飲食費の比較',
+                show: true,
+                fontFamily: 'ＭＳ ゴシック',
+                fontSize: '20px',
+                textAlign: 'center',
+                textColor: 'black',
+                location: 's',
+            }
+      }
+  );
+
+  jQuery.jqplot(
+      'march_eatcost_10and20',
+      [
+          eatCost_10_2year, eatCost_20_2year
+      ],
+      {
+          animate: true,
+
+          seriesDefaults: {
+              renderer: jQuery . jqplot . BarRenderer,
+
+              pointLabels: {
+                   show: true,
+                   location: 'n',
+                   ypadding: -3,
+                   escapeHTML: false,
+                   formatString: '<b style="color: blue;">%d</b>'
+               }
+          },
+          series: [
+             { label: '20歳未満' },
+             { label: '20代' },
+            //  { label: '30代'},
+            //  { label: '40代'},
+
+          ],
+          legend: {
+                show: true,
+                placement: 'outsideGrid',
+                location: 'e',
+                renderer: jQuery . jqplot . EnhancedLegendRenderer,
+                rendererOptions: {
+                    numberColumns: 3
+                }
+            },
+
+
+          seriesColors:[ '#FF0000', '#FFA500'],
+
+          axes: {
+              xaxis: {
+                  renderer: jQuery . jqplot . CategoryAxisRenderer,
+
+              },
+              // 縦軸(y軸)
+              yaxis:{
+                  label: '人数(人)',
+                  min: 0,
+                  max: 50,
+                  tickInterval: 10,
+              }
+          },
+          title: {
+                text: '3月の飲食費の比較(20代未満と20代)',
                 show: true,
                 fontFamily: 'ＭＳ ゴシック',
                 fontSize: '20px',
